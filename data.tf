d6fb218008b5a32c057c3cc6405bbeed62265e04
Mark Ferrell <mark.ferrell@aplaceformom.com>
2021-07-22 13:27:18 -0700

    feat: add sidecar support

--- data.tf
+++ data.tf
@@ -22,11 +22,16 @@ locals {
     }
   ]
 
-  template = [{
+  sidecars = [
+    for key in sort(keys(var.sidecars)) : {
+      name  = key
+      image = var.sidecars[key]
+    }
+  ]
+
+  primary = [{
     name      = var.name
     image     = var.image
-    cpu       = var.cpus
-    memory    = var.memory
     command   = var.command
     essential = true
     portMappings = [{
@@ -46,4 +51,6 @@ locals {
       }
     }
   }]
+
+  template = compact(concat(primary, sidecars))
 }
