f9d51bdf44f78f0bf9189d58051703bf0bdf67d2
apfm-rickym <rickym@aplaceformom.com>
2022-07-21 22:01:37 -0700

    Fix: overwrite ssm params if the params exist

--- ssm.tf
+++ ssm.tf
@@ -1,19 +1,22 @@
 resource "aws_ssm_parameter" "loadbalancer" {
-  name  = "/scry/${var.name}/lb"
-  type  = "String"
-  value = local.alarm_lb
+  name      = "/scry/${var.name}/lb"
+  type      = "String"
+  value     = local.alarm_lb
+  overwrite = true
 }
 
 resource "aws_ssm_parameter" "targetgroup" {
-  name  = "/scry/${var.name}/tg"
-  type  = "String"
-  value = local.alarm_tg
+  name      = "/scry/${var.name}/tg"
+  type      = "String"
+  value     = local.alarm_tg
+  overwrite = true
 }
 
 resource "aws_ssm_parameter" "service_level_settings" {
   for_each = var.service_level_settings
 
-  name  = "/scry/${var.name}/${each.key}"
-  type  = "String"
-  value = each.value
+  name      = "/scry/${var.name}/${each.key}"
+  type      = "String"
+  value     = each.value
+  overwrite = true
 }
